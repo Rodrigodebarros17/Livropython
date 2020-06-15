65f8448958bff380094299ecc323e4fc81c0e199
Rodrigodebarros17 <rodrigodebarros17@gmail.com>
2020-06-15 12:52:08 -0300

    cap 9

--- CAP9/9.30.py
+++ CAP9/9.30.py
@@ -1,4 +1,4 @@
-# Programa 9.8 - Geração de uma págia web a partir de um dicionário
+# Programa 9.8 - Geração de uma página web a partir de um dicionário
 filmes = {
     "Drama": ["Cidadão Kane", "O Poderoso Chefão"],
     "comédia": ["Tempos Modernos", "American Pie", "Dr. Dolittle"], 
@@ -16,6 +16,8 @@ with open("filmes.html", "w", encoding="utf-8") as página:
  """)
     for c, v in filmes.items():
         página.write(f"<h1>{c}</h1>\n")
+        página.write("<ul>\n")
         for e in v:
-            página.write(f"<h2>{e}</h2>\n")
+            página.write(f"<li>{e}</li>\n")
+        página.write("</ul>\n")
     página.write("</body></html>")

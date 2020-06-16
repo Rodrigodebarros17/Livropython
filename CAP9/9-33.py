dc3abd2920e2adddbb04037d9989324b17f418e4
Rodrigodebarros17 <rodrigodebarros17@gmail.com>
2020-06-16 12:41:41 -0300

    cap 9

---
+++ CAP9/9-33.py
@@ -0,0 +1,24 @@
+import os
+import os.path
+
+dir = input("Informe o diretório:")
+
+with open(f"{dir}\dir.html", "w", encoding="utf-8") as página:
+    página.write(f"""
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<h1>{dir}</h1>
+<ul>
+ """)
+    for c in os.listdir(dir):
+        if os.path.isfile(c):
+            if s[len(s)-3:len(s)] == "jpg" or s[len(s)-3:len(s)] == "png":
+                página.write(f"<li><a href=\"url\">{dir}\{c}</a></li>")
+            else:
+                página.write(f"<li>{dir}\{c}</li>")
+    página.write("</ul>\n")
+    página.write("</body></html>")

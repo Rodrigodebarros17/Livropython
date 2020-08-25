953fc9b35b546bb620c7517a0a061f46556b832c
Rodrigo de Barros <rodrigodebarros17@gmail.com>
2020-08-25 11:10:21 -0300

    CAP10

---
+++ CAP10/listaunica.py
@@ -0,0 +1,32 @@
+class ListaÚnica:
+    def __init__(self, elem_class):
+        self.lista = []
+        self.elem_class = elem_class
+    def __len__(self):
+        return len(self.lista)
+    def __iter__(self):
+        return iter(self.lista)
+    def __getitem__(self, p):
+        return self.lista[p]
+    def indiceVálido(self, i):
+        return i >= 0 and i < len(self.lista)
+    def adiciona(self, elem):
+        if self.pesquisa(elem) == -1:
+            self.lista.append(elem)
+    def remove(self, elem):
+        self.lista.remove(elem)
+    def pesquisa(self, elem):
+        self.verifica_tipo(elem)
+        try:
+            return self.lista.index(elem)
+        except ValueError:
+            return -1
+    def verifica_tipo(self, elem):
+        if not isinstance(type(elem), self.elem_class):
+            raise TypeError("Tipo Inválido")
+    def ordena(self, chave=None):
+        self.lista.sort(key=chave)
+
+
+
+
